a74bf521b837834b62c91d2579ae73aaae160d46
KayKiP <167833546+KayKiP@users.noreply.github.com>
2024-04-23 21:06:17 -0400

    Add files via upload

---
+++ Collection_Qs11/NumCollection.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Collection_Qs11
+{
+    public class NumCollection<T> where T : struct
+    {
+        private List<T> collection;
+        public NumCollection()
+        {
+            collection = new List<T>();
+        }
+
+        public void AddNum(T num)
+        {
+            collection.Add(num);
+        }
+
+        public T GetNum(int index)
+        {
+            if (index < 0 || index >= collection.Count)
+            {
+                throw new IndexOutOfRangeException("Index is out of range.");
+            }
+
+            return collection[index];
+        }
+
+        public List<T> DescNum()
+        {
+            List<T> sortCollection = collection.OrderByDescending(x => x).ToList();
+            return sortCollection;
+        }
+    }
+}
---
+++ Collection_Qs11/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Collection_Qs11
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            NumCollection<int> intNum = new NumCollection<int>();
+            intNum.AddNum(1);
+            intNum.AddNum(2);
+            intNum.AddNum(3);
+            intNum.AddNum(4);
+           
+            Console.WriteLine("Last Number is: " + intNum.GetNum(3));
+            
+            List<int> sortCollection = intNum.DescNum();
+            Console.WriteLine("Sorted numbers in descending order:");
+            foreach (var num in sortCollection)
+            {
+                Console.WriteLine(num);
+            }
+        }
+    }
+}
